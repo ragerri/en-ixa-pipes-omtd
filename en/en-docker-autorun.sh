74fd06b8e8a14aac8411f2e635ec72528bfd0a88
Rodrigo Agerri <ragerri@apache.org>
2018-03-21 23:50:58 +0100

    simple tokenizer run

--- en/en-docker-autorun.sh
+++ en/en-docker-autorun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat 2>/dev/null | java -jar ixa-pipe-tok-exec.jar tok -l en --inputkaf 2>/dev/null | java -jar ixa-pipe-pos-exec.jar tag -m pos.bin -lm lemma.bin 2>/dev/null | java -jar ixa-pipe-nerc-exec.jar tag -m brown.bin 2>/dev/null 2>/dev/null | java -jar ixa-pipe-chunk-exec.jar tag -m chunk.bin 2>/dev/null | java -jar ixa-pipe-doc-exec.jar tag -m doc.bin 2>/dev/null
+cat 2>/dev/null | java -jar ixa-pipe-tok-exec.jar tok -l en --inputkaf 2>/dev/null

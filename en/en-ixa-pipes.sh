8479c82a75a608c1cca7c3d3f38e44ec9133f009
Aitor Soroa <a.soroa@gmail.com>
2018-04-10 12:46:53 +0200

    use directoried for --input and --output

--- en/en-ixa-pipes.sh
+++ en/en-ixa-pipes.sh
@@ -3,6 +3,20 @@
 input=/dev/stdin
 output=/dev/stdout
 
+function run_file {
+	ifile=$1
+	ofile=$2
+	cat ${ifile} | \
+		./xmi2naf.py | \
+		java -jar ixa-pipe-tok-exec.jar tok -l en --inputkaf 2>/dev/null | \
+		java -jar ixa-pipe-pos-exec.jar tag -m pos.bin -lm lemma.bin 2>/dev/null | \
+		java -jar ixa-pipe-nerc-exec.jar tag -m nerc.bin 2>/dev/null 2>/dev/null | \
+		java -jar ixa-pipe-chunk-exec.jar tag -m chunk.bin 2>/dev/null | \
+		java -jar ixa-pipe-doc-exec.jar tag -m doc.bin 2>/dev/null | \
+		./naf2xmi.py > ${ofile}
+}
+
+declare -a POSITIONAL
 while [[ $# -gt 0 ]]; do
 	  key="$1"
 
@@ -24,11 +38,22 @@ while [[ $# -gt 0 ]]; do
 	  esac
 done
 
-cat ${input} | \
-	./xmi2naf.py | \
-	java -jar ixa-pipe-tok-exec.jar tok -l en --inputkaf 2>/dev/null | \
-	java -jar ixa-pipe-pos-exec.jar tag -m pos.bin -lm lemma.bin 2>/dev/null | \
-	java -jar ixa-pipe-nerc-exec.jar tag -m nerc.bin 2>/dev/null 2>/dev/null | \
-	java -jar ixa-pipe-chunk-exec.jar tag -m chunk.bin 2>/dev/null | \
-	java -jar ixa-pipe-doc-exec.jar tag -m doc.bin 2>/dev/null | \
-	./naf2xmi.py > ${output}
+if [ ! -d ${input} ]; then
+	echo "ERROR: --input must be a directory"
+	exit 1
+fi
+
+if [ ! -d ${output} ]; then
+	echo "ERROR: --output must be a directory"
+	exit 1
+fi
+
+declare -a IFILES
+IFILES=( $(ls ${input}/*.xmi) )
+for i in "${IFILES[@]}"; do
+	bfile=$(basename $i)
+	ifile=${input}/$bfile
+	ofile=${output}/$bfile
+	run_file ${ifile} ${ofile}
+done
+cp -f typesystem.xml ${output} >& /dev/null

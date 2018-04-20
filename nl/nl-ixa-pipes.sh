ab4e17c01e5695c4ce6fadd117c577438ba98686
Rodrigo Agerri <ragerri@apache.org>
2018-04-20 17:32:33 +0200

    dutch dockerfile added

---
+++ nl/nl-ixa-pipes.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+input=/dev/stdin
+output=/dev/stdout
+
+function run_file {
+	ifile=$1
+	ofile=$2
+	cat ${ifile} | \
+		/usr/bin/xmi2naf.py | \
+		java -jar /usr/bin/ixa-pipe-tok-exec.jar tok -l nl --inputkaf 2>/dev/null | \
+		java -jar /usr/bin/ixa-pipe-pos-exec.jar tag -m /usr/bin/pos.bin -lm /usr/bin/lemma.bin 2>/dev/null | \
+		java -jar /usr/bin/ixa-pipe-nerc-exec.jar tag -m /usr/bin/nerc.bin 2>/dev/null 2>/dev/null | \
+		/usr/bin/naf2xmi.py > ${ofile}
+}
+
+declare -a POSITIONAL
+while [[ $# -gt 0 ]]; do
+	  key="$1"
+
+	  case $key in
+		  --input)
+			  input="$2"
+			  shift # past argument
+			  shift # past value
+			  ;;
+		  --output)
+			  output="$2"
+			  shift # past argument
+			  shift # past value
+			  ;;
+		  *)    # unknown option
+			  POSITIONAL+=("$1") # save it in an array for later
+			  shift # past argument
+			  ;;
+	  esac
+done
+
+if [ ! -d ${input} ]; then
+	echo "ERROR: --input must be a directory"
+	exit 1
+fi
+
+if [ ! -d ${output} ]; then
+    mkdir ${output}
+    echo "Creating --output directory"
+fi
+
+declare -a IFILES
+IFILES=( $(ls ${input}/*.xmi) )
+for i in "${IFILES[@]}"; do
+	bfile=$(basename $i)
+	ifile=${input}/$bfile
+	ofile=${output}/$bfile
+	run_file ${ifile} ${ofile}
+done
+cp -f /usr/bin/typesystem.xml ${output} >& /dev/null
